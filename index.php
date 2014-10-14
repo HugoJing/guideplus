bbb855319d61a6d80b9894ce05fe72cb6353f6c7
HugoJing <toloric@163.com>
2014-10-14 11:47:20 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<?php
+
 <script src="https://cn.avoscloud.com/scripts/lib/av-0.4.3.min.js"></script>
 
 AV.initialize("jpi0gdoek99s47rgf6kif9uczcdz9mmzj3bfoo27bwmgw1hk", "u52y8nbcvu6a7qcag549yg5is1tep1lvqtvbk28zv46ki655");
 // 初始化 param1：应用 id、param2：应用 key
 
-<script type="text/javascript">
+
 var TestObject = AV.Object.extend("TestObject");
 var testObject = new TestObject();
 testObject.save({foo: "bar"}, {
@@ -12,6 +12,3 @@ testObject.save({foo: "bar"}, {
   alert("AVOS Cloud works!");
   }
 });
-</script>
-
-?>
