2ab0a3b040972b340b3dbb3fdcedcb3e9b21b85c
HugoJing <toloric@163.com>
2014-12-17 23:24:02 +0800

    update app.js

--- cloud/app.js
+++ cloud/app.js
@@ -22,47 +22,18 @@ app.get('/hello', function(req, res) {
 
 // 创建一个AV.Object子类Visitor.
 var Post = AV.Object.extend('Post');
-// 创建函数
-function renderIndex(res, post){
-	//创建一个 AV.Query 对象
-	var query = new AV.Query(Post);
-	//设置查询条件
-	query.skip(0);     //用 skip 跳过前面0个结果 / skip the first 0 results
-	query.limit(10);   //用设定 limit 的方法来限定返回的结果数,默认100 / limit to at most 10 results
-	query.descending('createdAt');  //控制返回结果的顺序 / Sorts the results in descending order by the createAt field
-	//用 find 方法返回一个满足条件的 AV.Object 数组
-	query.find({
-		success: function(results){
-			res.render('post',{ title: title, body: body});
-		},
-		error: function(error){
-			console.log(error);
-			res.render('500',500)
-		}
-	});
+var post = new Post();
+
+app.get('/post/:id', function(req, res) {
+    var id = req.params.id
+	res.render('post', { title: title });
+});
 
 }
 
 //使用express路由API服务 / 的http GET请求
 //使用express路由API服务 /的http POST请求
-app.git('post/:id',function(req, res){
-	var id = req.body.id;
-	if(name && name.trim() !=''){
-		//Save visitor
-		var visitor = new Visitor();
-		visitor.set('post', name);
-		visitor.save(null, {
-			success: function(gameScore) {
-				res.redirect('post',{title,title});
-			},
-			error: function(gameScore, error) {
-				res.render('500', 500);
-			}
-		});
-	}else{
-		res.redirect('/');
-	}
-});
+
 
 // This line is required to make Express respond to http requests.
 app.listen();
