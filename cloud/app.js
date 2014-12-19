5267f7b94ca644b028ca48623c4a12fad1a10e79
HugoJing <toloric@163.com>
2014-12-19 09:50:33 +0800

    update app.js

--- cloud/app.js
+++ cloud/app.js
@@ -4,12 +4,7 @@ var app = express();
 var avosExpressHttpsRedirect = require('avos-express-https-redirect');
 
 
-// App全局配置
-//设置模板目录
-if(__production)
-	app.set('views', 'cloud/views');
-else
-	app.set('views', 'cloud/dev_views');
+
 app.set('view engine', 'ejs');    // 设置template引擎
 app.use(avosExpressHttpsRedirect()); //启用HTTPS
 app.use(express.bodyParser());    // 读取请求body的中间件
@@ -19,15 +14,12 @@ app.get('/hello', function(req, res) {
 	res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
 
-// 创建一个AV.Object子类Visitor.
-var Post = AV.Object.extend('Post');
-
-app.get('/post/:id', function(req, res){
-	var id = req.query.id;
-	res.render('post',{ title: title, visitors: results});
+app.get('/', function (req, res) {
+  var q = req.query.q;
+  var md5Value = utility.md5(q);
+  res.send(md5Value);
 });
-//使用express路由API服务 / 的http GET请求
-//使用express路由API服务 /的http POST请求
+
 
 
 // This line is required to make Express respond to http requests.
