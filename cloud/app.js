7d82988cc4437bb3595d5f5e1dcd8915eac8cca3
HugoJing <toloric@163.com>
2014-12-23 10:56:40 +0800

    update app.js

--- cloud/app.js
+++ cloud/app.js
@@ -56,6 +56,7 @@ app.post('/admin/storys/new', function(req, res) {
     var content = req.body.content;
     //var image = req.body.image;
     if (title && content) {
+        var Story = AV.Object.extend('Story');
         var story = new AV.Object(Story);
         story.set('title', title);
         story.set('content', content);
