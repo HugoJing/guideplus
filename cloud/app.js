3ec205ad713910d62c1aba811e456e41abc1da21
HugoJing <toloric@163.com>
2014-12-30 16:43:43 +0800

    update app.js

--- cloud/app.js
+++ cloud/app.js
@@ -4,6 +4,10 @@
 // 在 Cloud code 里初始化 Express 框架
 var express = require('express');
 var app = express();
+var _ = require('underscore');
+var expressLayouts = require('express-ejs-layouts');
+var qn = require('qiniu');
+//var avosExpressHttpsRedirect = require('avos-express-https-redirect');
 
 var mstory = require('cloud/mstory.js');
 var mlog = require('cloud/mlog.js');
@@ -11,7 +15,12 @@ var mutil = require('cloud/mutil.js');
 
 app.set('views','cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
+//app.use(avosExpressHttpsRedirect());
 app.use(express.bodyParser());    // 读取请求 body 的中间件
+app.use(expressLayouts);
+
+qiniu.conf.ACCESS_KEY = 'e-_jowIUFJpcA9JlEO-VPRUjGQFRQI-MJ9-c4U0f'
+qiniu.conf.SECRET_KEY = 'z6bt7l15zt4koRYr-n26sXSGKJN4YV0Tych4-5av'
 
 var renderError = mutil.renderError;
 var renderErrorFn = mutil.renderErrorFn;
@@ -35,16 +44,19 @@ app.get('/story/:id', function (req, res) {
 });
 
 // 列表页
-app.get('/admin/storys', function (req, res) {
-    var query = new AV.Query('Story');
-    query.descending('createdAt');
-    query.limit(10);
-    query.find().then(function (storys) {
-      storys = storys || [];
-      storys = _.map(storys, storys);
-      res.render('list', {storys: storys});
-    }, mutil.renderErrorFn(res));
-  });
+// app.get('/admin/storys', function (req, res) {
+//   var skip = req.query.skip;
+//   var limit = 100;
+//   var query = new AV.Query("Story");
+//   query.skip(skip);
+//   query.descending("createdAt");
+//   query.find().then(function (storys) {
+//       storys = storys || [];
+//       //归属Ticket
+//       storys = _.map(storys, mstory.transformStory);
+//       res.render('admin_list', {storys: storys});
+//     }, renderErrorFn(res));
+// });
 
 // 录入页
 app.get('/admin/storys/new', function(req, res) {
@@ -52,11 +64,31 @@ app.get('/admin/storys/new', function(req, res) {
 });
 // 点击提交将触发下列函数
 app.post('/admin/storys/new', function (req, res) {
-    mstory.addCurStory(req.body).then(function () {
-      res.redirect("/story/" + story.id);
-    }, mutil.renderErrorFn(res));
-}
+    var story = new Story;
+    story.set('title', req.body.title);
+    story.set('type', req.body.type);
+    story.set('content', req.body.content);
+    story.set('readLink', req.body.readLink);
+    story.set('media', req.body.media);
+    story.save(null, {
+        success: function(story) {
+            story.set('url', 'http://guideplus.me/story/' + story.id);
+            story.save();
+            console.log('New object created with objectId: ' + story.id);
+            renderInfo(res, '<p>成功！辛苦了，你又录入了一篇帖子： ' + '<a href="http://guideplus.me/story/' + story.id +'" target="_blank" title="点击查看">点击查看</a></p>')
+        },
+        error: function(story, error) {
+            console.log('Failed to create new object, with error code: ' + error.description);
+            renderErrorFn(res);
+        }
+    });
+})
 
+//     mstory.createStory(req.body).then(function () {
+//         console.log("新建了一条帖子");
+//         //res.redirect('/admin/storys/new');
+//     }, mutil.renderErrorFn(res));
+// });
 
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
 app.listen();
